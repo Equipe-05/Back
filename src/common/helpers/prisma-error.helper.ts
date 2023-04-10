0ae228903774b5ad393145757a62dfd05fe6d2c0
Flavio Alvarenga <flavio.alva@outlook.com>
2023-04-10 20:34:30 -0300

    feat(helper): rework `nestError` error message

--- src/common/helpers/prisma-error.helper.ts
+++ src/common/helpers/prisma-error.helper.ts
@@ -2,58 +2,33 @@ import { Error } from '../types/types';
 import { nestErrorHelper } from './nest-error.helper';
 
 export function prismaExceptionHelper(error: Error) {
-  // if (error.code === 'P2002') {
-  //   nestErrorHelper({
-  //     name: 'UnprocessableEntityError',
-  //     message: error?.meta?.target ?? 'Data already exists.',
-  //   });
-  // } else if (error.code === 'P2025') {
-  //   nestErrorHelper({
-  //     name: 'NotFoundError',
-  //     message: error?.meta?.cause ?? 'Data not found.',
-  //   });
-  // } else if (error.code === 'P2014') {
-  //   nestErrorHelper({
-  //     name: 'UnprocessableEntityError',
-  //     message: error?.meta?.cause ?? 'Unique constraint failed.',
-  //   });
-  // } else {
-  //   nestErrorHelper({
-  //     name: 'default',
-  //     message: error?.meta?.cause ?? error?.message ?? 'Bad Request',
-  //   });
-  // }
-
   const errorMap = new Map([
     [
       'P2002',
       {
         name: 'UnprocessableEntityError',
-        message: (error: Error) =>
-          error?.meta?.target ?? 'Data already exists.',
+        message: error?.meta?.target ?? 'Data already exists.',
       },
     ],
     [
       'P2025',
       {
         name: 'NotFoundError',
-        message: (error: Error) => error?.meta?.cause ?? 'Data not found.',
+        message: error?.meta?.cause ?? 'Data not found.',
       },
     ],
     [
       'P2014',
       {
         name: 'UnprocessableEntityError',
-        message: (error: Error) =>
-          error?.meta?.cause ?? 'Unique constraint failed.',
+        message: error?.meta?.cause ?? 'Unique constraint failed.',
       },
     ],
     [
       'default',
       {
         name: 'default',
-        message: (error: Error) =>
-          error?.meta?.cause ?? error?.message ?? 'Bad Request',
+        message: error?.meta?.cause ?? error?.message ?? 'Bad Request',
       },
     ],
   ]);
@@ -62,6 +37,6 @@ export function prismaExceptionHelper(error: Error) {
 
   nestErrorHelper({
     name: errorObject.name,
-    message: errorObject.message(error),
+    message: errorObject.message,
   });
 }
