8d979c65b28376d6825ae04c3b71937d59078072
Enrique Molina <76474854+enriquemolina61@users.noreply.github.com>
2023-04-15 20:26:45 -0300

    init: dto-create

--- src/customer/dto/create-customer.dto.ts
+++ src/customer/dto/create-customer.dto.ts
@@ -1,1 +1,21 @@
-export class CreateCustomerDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+
+export class CreateCustomerDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(50)
+  @ApiProperty({
+    description: 'The name of the user',
+    example: 'John Doe',
+  })
+  readonly name: string;
+  readonly cnpj: string;
+  readonly address: string;
+  readonly phone: string;
+  readonly franchise: string;
+  readonly franchiseId: string;
+  readonly sales: string;
+  readonly products: string;
+}
