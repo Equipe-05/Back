6034731c6420b8361f66adc678ed8c7cc8b98bd6
Flavio Alvarenga <flavio.alva@outlook.com>
2023-04-17 15:20:15 -0300

    feat(util): add `createUserRole` Map

---
+++ src/common/util/create-user-role.ts
@@ -0,0 +1,9 @@
+import { Role } from '@prisma/client';
+
+export const createUserRole = new Map<Role | 'default', Role>([
+  [Role.EMPLOYEE, Role.EMPLOYEE],
+  [Role.FRANCHISEE, Role.EMPLOYEE],
+  [Role.OPERATOR, Role.FRANCHISEE],
+  [Role.MANAGER, Role.OPERATOR],
+  ['default', Role.EMPLOYEE],
+]);
