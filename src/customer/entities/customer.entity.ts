8e319c2c2869e8b4883dd8d237ac59bea73feae9
Enrique Molina <76474854+enriquemolina61@users.noreply.github.com>
2023-04-19 19:25:43 -0300

    Update src/customer/entities/customer.entity.ts
    Co-authored-by: Flávio Alvarenga <13731643+alvalenda@users.noreply.github.com>

--- src/customer/entities/customer.entity.ts
+++ src/customer/entities/customer.entity.ts
@@ -4,7 +4,7 @@ export class Customer {
   address: string;
   cnpj: string;
   phone: string;
-  franchises?: string[];
+  franchises?: Franchise[];
   sales?: string[]; // ! Sale is not implemented yet
   products?: string[];
 }
