7b7a77715c87ed1e82e1d4d0c88d9a207cc4f891
Enrique Molina <76474854+enriquemolina61@users.noreply.github.com>
2023-04-19 19:27:37 -0300

    Update src/customer/entities/customer.entity.ts
    Co-authored-by: Flávio Alvarenga <13731643+alvalenda@users.noreply.github.com>

--- src/customer/entities/customer.entity.ts
+++ src/customer/entities/customer.entity.ts
@@ -1,3 +1,6 @@
+import { Franchise } from "src/franchise/entities/franchise.entity";
+import { Product } from "src/product/entities/product.entity";
+
 export class Customer {
   id: string;
   name: string;
